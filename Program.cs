cc7b335d728d9adfb900a5f83e135b8e33dd554d
olofs22 <olof.business.svensson@gmail.com>
2025-10-15 18:22:29 +0200

    Lagt till en metod för att ta bort transaktioner. Lagt till lite färg styling.

--- Program.cs
+++ Program.cs
@@ -4,23 +4,29 @@
     {
         static void Main(string[] args)
         {
+            //skapar instanser av klasserna
             TransactionManager transactionManager = new TransactionManager();
             Meny meny = new Meny();
+
+            //running bool för att kunna kontrollera hur programmet körs
             bool running = true;
 
             while (running)
             {
-                meny.WriteMenu();
+                meny.WriteMenu(); //kör meny metoden
 
-                int val = Convert.ToInt32(Console.ReadLine());
+                int val = Convert.ToInt32(Console.ReadLine()); //tar in en input som används för att navigera menyn
 
-                switch (val)
+                switch (val) //switch för att navigera programmet
                 {
                     case 1:
                         transactionManager.AddTransaction();
                         break;
                     case 2:
                         transactionManager.ListTransactions();
+                        break;
+                    case 4:
+                        transactionManager.DeleteTransaction();
                         break;
                     case 5:
                         running = false;
--- Transaction.cs
+++ Transaction.cs
@@ -6,23 +6,29 @@ using System.Threading.Tasks;
 
 namespace Inlämning2
 {
+    //Skapa klass transaktion som innehåller attibuterna transkationerna behöver.
     internal class Transaction
     {
+        
         public string Description { get; set; }
         public double Amount { get; set; }
         public string Category { get; set; }
         public DateTime Date { get; set; }
+        public int TransID { get; set; }
 
+        //En tom konstruktor för att få programmet att fungera
         public Transaction()
         {
 
         }
-        public Transaction(string description, double amount, string category, DateTime date)
+        //Konstruktor med attributen
+        public Transaction(string description, double amount, string category, DateTime date, int transid)
         {
             Description = description;
             Amount = amount;
             Category = category;
             Date = date;
+            TransID = transid;
         }
     }
 
