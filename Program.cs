1c2190ba3a02e03d49f5cad4520e96d7a5830404
olofs22 <olof.business.svensson@gmail.com>
2025-10-15 17:23:30 +0200

    lagt till meny, switch och funktion som avslutar

--- Program.cs
+++ Program.cs
@@ -5,11 +5,33 @@
         static void Main(string[] args)
         {
             TransactionManager transactionManager = new TransactionManager();
-            
+            Meny meny = new Meny();
+            bool running = true;
+
+            while (running)
+            {
+                meny.WriteMenu();
+
+                int val = Convert.ToInt32(Console.ReadLine());
+
+                switch (val)
+                {
+                    case 1:
+                        transactionManager.AddTransaction();
+                        break;
+                    case 2:
+                        transactionManager.ListTransactions();
+                        break;
+                    case 5:
+                        running = false;
+                        break;
+                }
+            }
+
             
 
-            transactionManager.AddTransaction();
-            transactionManager.ListTransactions();
+            
+            
         }
     }
 }
